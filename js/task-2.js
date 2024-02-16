f4b4b278d241891c5e7351dcf640f882e19fa110
TaniaFed <ktv.poshta@gmail.com>
2024-02-15 22:41:23 -0500

    hw-3(task 2)

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,13 @@
+function makeArray(firstArray, secondArray, maxLength) {
 
+    const newArray = firstArray.concat(secondArray);
+
+    if (newArray.length > maxLength) {
+        return newArray.slice(0, maxLength);
+    } else {
+        return newArray;
+    }
+}
 
 
 
